3b1191fef1a457d8efb03fdb503d0c25c44a3e91
gp-kato <kato@gp1000.jp>
2024-05-10 17:36:21 +0900

    web.phpを補完

--- src/gp-laravel-board/routes/web.php
+++ src/gp-laravel-board/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BbsController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CsvDownloadController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,17 +12,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/',[BbsController::class,'index']);
 Route::post('/bbs_add',[BbsController::class,'add']);
 
-Route::get('/login',[LoginController::class,'login']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::group(['middleware' => 'auth'], function() {
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::post('/admin',[BbsController::class,'admin']);
 
     Route::get('/edit/{message}',[BbsController::class,'edit']);
@@ -31,3 +38,5 @@ Route::group(['middleware' => 'auth'], function() {
 
     Route::get('/delete/{message}',[BbsController::class,'delete']);
 });
+
+require __DIR__.'/auth.php';
