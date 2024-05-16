8f36b195a6862393a635f909f8856bc0d5f6e36e
gp-kato <kato@gp1000.jp>
2024-05-16 13:25:41 +0900

    ログイン機能の実装

--- src/gp-laravel-board/resources/views/admin.blade.php
+++ src/gp-laravel-board/resources/views/admin.blade.php
@@ -6,38 +6,37 @@
 
 @section('content')
     <section>
-        @if ( !empty(session('admin_login')) && session('admin_login') === true )
-            <form method="get" action="csv-download">
-                @csrf
-                <select name="limit">
-                    <option value="">全て</option>
-                    <option value="10">10件</option>
-                    <option value="30">30件</option>
-                </select>
-                <input type="submit" name="btn_download" value="ダウンロード">
-            </form>
-            <hr>
-            <div class="bodywrapper">
-                @foreach ($bbs_data as $data)
-                    <div class="messageRow">
-                        <div class="message">
-                            <section>
-                                <article>
-                                    <div class="info">
-                                        <h2>{{ $data->view_name }}</h2>
-                                        <time>{{ $data->post_date }}</time>
-                                        <p><a href="edit/<?= htmlspecialchars($data->id) ?>">編集</a>  <a href="delete/<?= htmlspecialchars($data->id) ?>">削除</a></p>
-                                    </div>
-                                    <p>{{ $data->message }}</p>
-                                </article>
-                            </section>
-                        </div>
+        <form method="get" action="csv-download">
+            @csrf
+            <select name="limit">
+                <option value="">全て</option>
+                <option value="10">10件</option>
+                <option value="30">30件</option>
+            </select>
+            <input type="submit" name="btn_download" value="ダウンロード">
+        </form>
+        <hr>
+        <div class="bodywrapper">
+            @foreach ($bbs_data as $data)
+                <div class="messageRow">
+                    <div class="message">
+                        <section>
+                            <article>
+                                <div class="info">
+                                    <h2>{{ $data->view_name }}</h2>
+                                    <time>{{ $data->post_date }}</time>
+                                    <p><a href="edit/<?= htmlspecialchars($data->id) ?>">編集</a>  <a href="delete/<?= htmlspecialchars($data->id) ?>">削除</a></p>
+                                </div>
+                                <p>{{ $data->message }}</p>
+                            </article>
+                        </section>
                     </div>
-                @endforeach
-            </div>
-            <form method="get" action="">
-                <input type="submit" name="btn_logout" value="ログアウト">
-            </form>
-        @endif
+                </div>
+            @endforeach
+        </div>
+        <form method="post" action="logout">
+            @csrf
+            <input type="submit" name="btn_logout" value="ログアウト">
+        </form>
     </section>
 @endsection
